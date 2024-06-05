87732721375a59a82a92a80c5031ba4934e5ef61
DulceST <ladroladebros864@gmail.com>
2024-06-04 19:43:33 -0600

    Seagrego Order Card

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,3 +1,4 @@
+import OrderCard from "@/components/order/OrderCard";
 import Heading from "@/components/ui/heading";
 import { prisma } from "@/src/lib/prisma";
 
@@ -5,6 +6,13 @@ async function getPendingOrders() {
     const orders = await prisma.order.findMany({
         where: {
             status: false
+        }, 
+        include:{
+            OrderProducts: {
+                include: {
+                    product: true
+                }
+            }
         }
     })
 
@@ -13,13 +21,18 @@ async function getPendingOrders() {
 
 export default async function OrdersPage(){
     const orders = await getPendingOrders()
-    console.log(orders)
     
     return(
         <>
-        <Heading>
-            Administrar Ordenes
-        </Heading>
+        <Heading>Administrar Ordenes</Heading>
+        {orders.length ? (
+            <div className="grid grid-cols-1 lg: grid-cols-2 2xl:grid-cols3 gap-5 mt-5">
+                {orders.map(order => (
+                    <OrderCard />
+                ))}
+
+            </div>
+        ) : <p className="text-center">No hay ordenes pendientes</p>}
         </>
     )
 }
