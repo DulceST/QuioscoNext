6e521021e960e848259b8283104b3f55b611c462
DulceST <ladroladebros864@gmail.com>
2024-06-05 19:51:27 -0600

    Proyecto listo

--- components/products/AppProductForm.tsx
+++ components/products/AppProductForm.tsx
@@ -1,54 +1,55 @@
 
-"use client"
-import React, { Children } from "react";
+"use client";
+import React from "react";
+import ProductForm from "./ProductForm";
 import { ProductSchema } from "@/src/schema";
 import { toast } from "react-toastify";
 import { createProduct } from "@/actions/create-product-action";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
-export default function AppProductForm({ children }: { children: React.ReactNode }) {
-    const router = useRouter()
+export default function AddProductForm({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const router = useRouter();
 
-    const handlerSubmit = async (formData: FormData) => {
-        const data = {
-            name: formData.get('name'),
-            price: formData.get('price'),
-            categoryId: formData.get('categoryId'),
-            image: formData.get('image')
-
-        }
-        const result = ProductSchema.safeParse(data)
-        if(!result.success){
-            result.error.issues.forEach(issue => {
-                toast.error(issue.message)
-            })
-            return
-        }
-        const response = await createProduct(result.data)
-        if(response?.errors){
-            response.errors.forEach(issue => {
-                toast.error(issue.message)
-            })
-            return
-        }
-        toast.success('Producto creado correctamente')
-        router.push('/admin/products')
+  const handleSubmit = async (formData: FormData) => {
+    const data = {
+      name: formData.get("name"),
+      price: formData.get("price"),
+      categoryId: formData.get("categoryId"),
+      image: formData.get("image"),
+    };
+    const result = ProductSchema.safeParse(data);
+    if (!result.success) {
+      result.error.issues.forEach((issue) => {
+        toast.error(issue.message);
+      });
+      return;
     }
 
-    return (
-        <div className="bg-white mt-10 px-5 py-10 rounded-md shadow-md max-w-3xl mx-auto">
-            <form className="space-y-5"
-                action={handlerSubmit}>
-
-                {children}
-
-                <input
-                    type="submit"
-                    className="bg-indigo-600 hover:bg-indigo-800 text-white w-full p-3 uppercase font-bold cursor-pointer"
-                    value='Registrar producto' />
-
-            </form>
-        </div>
-    )
-
+    const response = await createProduct(result.data);
+    if (response?.errors) {
+      response.errors.forEach((issue) => {
+        toast.error(issue.message);
+      });
+      return;
+    }
+    toast.success("Producto Creado Correctamente");
+    router.push("/admin/products");
+  };
+  return (
+    <div className="bg-white mt-10 px-5 py-10 rounded-md shadow-md max-w-3xl mx-auto">
+      <form className="space-y-5" action={handleSubmit}>
+        {children}
+        <input
+          type="submit"
+          className="bg-indigo-800 hover:bg-indigo-950 text-white 
+          w-full mt-5 p-3 uppercase font-bold cursor-pointer"
+          value="Resgitrar Producto"
+        />
+      </form>
+    </div>
+  );
 }
